b63dd18b1c309fa603b3064f0ac89266c87764a9
Luminted <rr.levente@gmail.com>
2020-05-26 22:51:04 +0200

    [resolution-scaling]: changing card dimensions integer divisble

--- src/config/cardTypeConfigs.ts
+++ src/config/cardTypeConfigs.ts
@@ -2,7 +2,7 @@ import { FrenchCardConfig, CardConfig, CardTypes } from "../types/dataModelDefin
 
 const frenchCardConfig: FrenchCardConfig = {
     baseHeight: 88,
-    baseWidth: 63,
+    baseWidth: 64,
     cardRange: [2,3,4,5,6,7,8,9,10,'J','Q','K','A'],
     suits: ['Heart', 'Club', 'Spade', 'Diamond']
 }
